8f39624c321a0adc572f6e3df4b5a5d17546f18c
fabiolafrancoc-lab <fabiola.franco@bopidea.com>
2025-11-18 21:08:45 -0500

    Fix: change require to import for ES modules compatibility

--- api/send-whatsapp-codes.js
+++ api/send-whatsapp-codes.js
@@ -1,5 +1,5 @@
 // Enviar códigos de acceso por WhatsApp usando Twilio
-const twilio = require('twilio');
+import twilio from 'twilio';
 
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
